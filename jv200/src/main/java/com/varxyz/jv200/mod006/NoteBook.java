dbafb8f6b52d887ebf8f3146b0796e433735b062
workhan0918 <workhan0918@gmail.com>
2022-06-14 11:15:00 +0900

    init
    init

--- jv200/src/main/java/com/varxyz/jv200/mod006/NoteBook.java
+++ jv200/src/main/java/com/varxyz/jv200/mod006/NoteBook.java
@@ -1,5 +1,12 @@
 package com.varxyz.jv200.mod006;
 
-public class NoteBook {
+public class NoteBook extends Product{
 	
+	public NoteBook(String name) {
+		super(name);
+	}
+	
+	public double getPrice() {
+		return 25000.0;
+	}
 }
