665093bd823d9f71a174af8d05803589b563d7c7
workhan0918 <workhan0918@gmail.com>
2022-06-15 17:53:12 +0900

    java study
    java study

---
+++ jv200/src/main/java/com/varxyz/jv200/mod007/Cat.java
@@ -0,0 +1,9 @@
+package com.varxyz.jv200.mod007;
+
+public class Cat extends Animal {
+	
+	@Override
+	public void makeSound() {
+		System.out.println("야옹");
+	}
+}
---
+++ jv200/src/main/java/com/varxyz/jv200/mod007/Dog.java
@@ -0,0 +1,9 @@
+package com.varxyz.jv200.mod007;
+
+public class Dog extends Animal {
+	
+	@Override
+	public void makeSound() {
+		System.out.println("멍멍");
+	}
+}
